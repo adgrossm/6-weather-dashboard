b89a079f8805a3a145e328285df650cb80e2b25c
Adam Grossman <adgrossm@gmail.com>
2020-08-12 19:04:35 -0400

    set the <div> for each element like city, temp, humidity, etc.. still need to adjust text length and formatting but it works when you add a new city into the search field. It also keeps adding the city names as you search. it also calls the city from the past search list

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,4 +1,5 @@
 var cityName = [];
+var date = moment().format("MMM Do YYYY");
 
 $("#searchForm").on("submit", function (event) {
     event.preventDefault();
@@ -23,17 +24,31 @@ function searchForCity(city) {
         method: "GET"
     }).then(function (response) {
         console.log(response)
+        $(".jumbotron").empty()
         // create a var to store Humidity
         var humidity = (response.main.humidity)
-        var currentTemp = (((response.main.temp) - 273) * 1.8 +32)
         // create var to change Kelvin to Far
-        console.log(currentTemp)
+        var currentTemp = (((response.main.temp) - 273) * 1.8 +32)
+        //  var for weather icon
+        var weatherIcon = (response.weather[0].icon)
+        console.log(response.weather[0].icon)
+        // var for wind speed might need to convert to mph and chop the last 3 digits
+        var windSpeed = ((response.wind.speed)*2.237)
+        // see below around line 50 for the var for the uv index
+        
+        // console.log(windSpeed)
         // coord: {lat: 29.7633, lon: -95.3633} example
         var latitude = (response.coord.lat) 
-        console.log(latitude)// From response
+        // console.log(latitude)// From response
         var longitude = (response.coord.lon) // From response
-        console.log(longitude)
+        // console.log(longitude)
         // variable to store the uv for the location based on latitude and longitue
+        var cityDiv = $("<h2>").text(city + "  (" + date + ")") ;
+        var tempDiv = $("<h4>").text("Temperature: " + currentTemp + " \u00B0" + "F");
+        var humidityDiv = $("<h4>").text("Humidity: " + humidity+"%");
+        var windDiv = $("<h4>").text("Wind Speed: " + windSpeed+" MPH");
+        
+        $(".jumbotron").append(cityDiv, tempDiv, humidityDiv, windDiv);
 
         var uvURL = BASE_ROUTE + "uvi" + UV_API_KEY + latitude + "&lon=" + longitude
         // var uvURL = BASE_ROUTE + "forecast?q=" + latitude + longitude + API_KEY
@@ -41,11 +56,11 @@ function searchForCity(city) {
             url: uvURL,
             method: "GET"
         }).then(function (response) {
-            console.log(response)
-            // console.log UV index
-            console.log(response.value)
-
-
+            // var for uvIndex to get passed into current weather jumbotron
+            var uvIndex = (response.value)
+            console.log(uvIndex)
+            var uvDiv = $("<h4>").text("UV Index: " + uvIndex);
+            $(".jumbotron").append(uvDiv);
         })
     })
     // this is the forecast call
